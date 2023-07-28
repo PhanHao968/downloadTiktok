0aa7e38ab87b81f2aa63b0a6cd9b2dec6de862cf
Mikeconal <44361757+maithedung@users.noreply.github.com>
2023-07-28 11:49:52 +0700

    fix bug code

--- tiktok.py
+++ tiktok.py
@@ -5,7 +5,6 @@
 from datetime import datetime
 import pyperclip
 import re
-import clipboard
 import requests
 import flet as flet
 from flet import (
@@ -28,10 +27,161 @@ from flet import (
     CrossAxisAlignment,
 )
 
+
 class TiktokDownload(UserControl):
-    def __int__(self):
-        super().__int__()
-    #
+    def __init__(self):
+        super().__init__()
+
+    def build(self):
+        title = ResponsiveRow(
+            alignment="center",
+            controls=[
+                Container(
+                    col={"xs": 12, "sm": 10, "md": 10, "xl": 12},
+                    alignment=alignment.top_center,
+                    padding=30,
+                    content=Text(
+                        'Tải Video TikTok Miễn Phí.',
+                        size=40,
+                        weight="w600",
+                        text_align="center",
+                    ),
+                )
+            ],
+        )
+
+        nav = Container(
+            ResponsiveRow([
+                Column(col={"xs": 12, "sm": 12, "md": 12, "xl": 12},
+                       controls=[
+                           Container(
+                               padding=20,
+                               bgcolor="#DEDEDE",
+                               content=Row([
+                                   CircleAvatar(
+                                       width=60,
+                                       height=60,
+                                       bgcolor="white",
+                                       content=
+                                       Text("TD",
+                                            size=30,
+                                            weight="w900",
+                                            color="black",
+                                            text_align="center",
+                                            ),
+                                   ),
+                               ],
+                                   alignment="spaceBetween")
+                           )
+                       ]
+                       )
+            ]),
+        )
+
+        self.url_tiktok_textField = TextField(
+            border=InputBorder.NONE,
+            content_padding=padding.only(
+                top=0,
+                bottom=0,
+                right=20,
+                left=20
+            ),
+            hint_style=TextStyle(
+                size=16, color='#9DB2BF'
+            ),
+            text_style=TextStyle(
+                size=18,
+                color='black',
+            ),
+            hint_text='Dán liên kết Tiktok vào đây',
+            cursor_color='black',
+            width=386,
+            height=50,
+            col={"sm": 8, "md": 12, "lg": 10, "xl": 12},
+        )
+
+        self.url_change = Container(
+            height=49,
+            width=70,
+            bgcolor="blue600",
+            border_radius=10,
+            padding=0,
+            content=TextButton(
+                "Paste",
+                on_click=self.paste_click,
+                height=50
+            ),
+        )
+
+        url_download = Container(
+            # col={"sm": 10, "md": 8, "lg": 1, "xl": 1},
+            visible=True,
+            height=50,
+            width=90,
+            bgcolor="blue600",
+            border_radius=10,
+            padding=0,
+            content=TextButton(
+                "Download",
+                on_click=self.download_video,
+                height=50,
+                width=70
+            ),
+        )
+
+        container_input = Container(
+            alignment=alignment.center,
+            bgcolor="white",
+            padding=2,
+            border_radius=10,
+            col={"sm": 10, "md": 10, "lg": 6, "xl": 5},
+            content=Row([
+                self.url_tiktok_textField,
+                self.url_change,
+            ],
+                alignment="spaceBetween"
+            ),
+            # on_hover=self.on_button,
+        )
+
+        container_download = Container(
+            # alignment=alignment.center,
+            bgcolor="white",
+            padding=2,
+            border_radius=10,
+            col={"sm": 10, "md": 10, "lg": 1.2, "xl": 1},
+            content=url_download
+        )
+
+        item_row = ResponsiveRow(
+            alignment="center",
+            controls=[
+                container_input,
+                container_download
+            ]
+        )
+
+        container_item = Container(
+            alignment=alignment.center,
+            padding=20,
+            content=item_row
+        )
+
+        main_col = Column(
+            horizontal_alignment="center",
+            scroll="auto",
+            controls=[
+                nav,
+                Container(
+                    padding=padding.only(top=75)
+                ),
+                title,
+                container_item
+            ]
+        )
+
+        return main_col
+
     def download_video(self, e):
         url = "https://tiktok-downloader-download-tiktok-videos-without-watermark.p.rapidapi.com/vid/index"
         video_url = self.url_tiktok_textField.value
@@ -56,179 +206,28 @@ class TiktokDownload(UserControl):
         r = requests.get(url_video)
         open(name, "wb").write(r.content)
 
-    #
     def paste_click(self, e):
         url_tiktok = pyperclip.paste()
         self.url_tiktok_textField.value = url_tiktok
+        self.url_change.content = TextButton(
+            "Delete",
+            on_click=self.delete_click,
+            height=50
+        )
         self.update()
 
-    #
     def delete_click(self, e):
         self.url_tiktok_textField.value = ""
+        self.url_change.content = TextButton(
+            "Paste",
+            on_click=self.paste_click,
+            height=50
+        )
         self.update()
-
-    #
-    def on_button(self, e):
-        str_input = str(self.url_tiktok_textField.value)
-
-        if str_input == "":
-            self.url_paste.visible = True
-            self.url_paste.update()
-            self.url_delete.visible = False
-            self.url_delete.update()
-        else:
-            self.url_paste.visible = False
-            self.url_paste.update()
-            self.url_delete.visible = True
-            self.url_delete.update()
-
-        return self.on_button
-
-    def build(self):
-        _nav = Container(
-            ResponsiveRow([
-                Column(col={"xs": 12, "sm": 12, "md": 12, "xl": 12},
-                       controls=[
-                           Container(
-                               padding=20,
-                               bgcolor="#DEDEDE",
-                               content=Row([
-                                   CircleAvatar(
-                                       width=60,
-                                       height=60,
-                                       bgcolor="white",
-                                       content=
-                                       Text("TD",
-                                            size=30,
-                                            weight="w900",
-                                            color="black",
-                                            text_align="center",
-                                            ),
-                                   ),
-                               ], alignment="spaceBetween")
-                           )
-                       ]
-                       )
-            ]),
-        )
-
-        # titles
-        _title = ResponsiveRow(
-            alignment="center",
-            controls=[
-                Container(
-                    col={"xs": 12, "sm": 10, "md": 10, "xl": 12},
-                    alignment=alignment.top_center,
-                    padding=30,
-                    content=Text('Tải Video TikTok Miễn Phí.',
-                                 size=40,
-                                 weight="w600",
-                                 text_align="center",
-                                 ),
-                )
-            ],
-        )
-
-        #
-        url_tiktok_textField = TextField(
-            border=InputBorder.NONE,
-            content_padding=padding.only(
-                top=0, bottom=0, right=20, left=20),
-            hint_style=TextStyle(
-                size=16, color='#9DB2BF'
-            ),
-            text_style=TextStyle(
-                size=18,
-                color='black',
-            ),
-            hint_text='Dán liên kết Tiktok vào đây',
-            cursor_color='black',
-            width=386,
-            height=50,
-            col={"sm": 8, "md": 12, "lg": 10, "xl": 12},
-        )
-
-        #
-        url_paste = Container(
-            # col={"sm": 2, "md": 2, "lg": 2, "xl": 2},
-            visible=True,
-            height=49,
-            width=70,
-            bgcolor="blue600",
-            border_radius=10,
-            padding=0,
-            content=TextButton("Paste", on_click=self.paste_click, height=50),
-        )
-        url_delete = Container(
-            visible=False,
-            height=49,
-            width=70,
-            bgcolor="blue600",
-            border_radius=10,
-            content=TextButton("Delete", on_click=self.delete_click, height=50)
-        )
-        url_download = Container(
-            # col={"sm": 10, "md": 8, "lg": 1, "xl": 1},
-            visible=True,
-            height=50,
-            width=90,
-            bgcolor="blue600",
-            border_radius=10,
-            padding=0,
-            content=TextButton("Download", on_click=self.download_video, height=50, width=70),
-        )
-
-        # 7/6/23
-        self._item = [url_paste, url_delete]
-        self._item_row = ResponsiveRow(alignment="center")
-
-        _container_input = Container(
-            alignment=alignment.center,
-
-            bgcolor="white",
-            padding=2,
-            border_radius=10,
-            col={"sm": 10, "md": 10, "lg": 6, "xl": 5},
-            content=Row([
-                url_tiktok_textField,
-                url_paste,
-                url_delete,
-            ], alignment="spaceBetween"),
-            on_hover=self.on_button,
-        )
-
-        #
-        _container_download = Container(
-            # alignment=alignment.center,
-            bgcolor="white",
-            padding=2,
-            border_radius=10,
-            col={"sm": 10, "md": 10, "lg": 1.2, "xl": 1},
-            content=url_download
-        )
-
-        _container_item = Container(
-            alignment=alignment.center,
-            padding=20,
-            content=self._item_row
-        )
-
-        self._item_row.controls.append(_container_input)
-        self._item_row.controls.append(_container_download)
-
-        # main column
-        self._main_col = Column(horizontal_alignment="center", scroll="auto")
-        self._main_col.controls.append(_nav)
-        self._main_col.controls.append(Container(padding=padding.only(top=75)))
-        self._main_col.controls.append(_title)
-        self._main_col.controls.append(_container_item)
-        return self._main_col
 
 
 def main(page: Page):
-
-    _main = Container(
-        # height=self.page.height,
+    app = Container(
         expand=True,
         margin=-10,
         gradient=LinearGradient(
@@ -246,8 +245,9 @@ def main(page: Page):
             ]
         ),
     )
-    page.add(_main)
-    page.update()
+
+    page.add(app)
+
 
 if __name__ == "__main__":
     flet.app(target=main, view=flet.WEB_BROWSER)
